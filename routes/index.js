e324178f869421ac820c47a8a412d6f7542b938b
Thibaud Veyssade <tveyssade@gmail.com>
2023-12-07 11:22:16 +0100

    l

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+const uniqid = require("uniqid");
+
+router.post("/upload", async (req, res) => {
+  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const resultMove = await req.files.photoFromFront.mv(photoPath);
+
+  if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    fs.unlinkSync(photoPath);
+    res.json({ result: true, url: resultCloudinary.secure_url });
+  } else {
+    res.json({ result: false, error: resultMove });
+  }
+});
+
+module.exports = router;
